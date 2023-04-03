da42553736bbad7b3c8e779252db1a12e5cd73ff
YI <uuuuuu@protonmail.com>
2023-04-03 15:48:28 +0800

    play with dump memory

---
+++ contrib/elf.py
@@ -0,0 +1,64 @@
+def decode_funcname(dwarfinfo, address):
+    # Go over all DIEs in the DWARF information, looking for a subprogram
+    # entry with an address range that includes the given address. Note that
+    # this simplifies things by disregarding subprograms that may have
+    # split address ranges.
+    for CU in dwarfinfo.iter_CUs():
+        for DIE in CU.iter_DIEs():
+            try:
+                if DIE.tag == 'DW_TAG_subprogram':
+                    lowpc = DIE.attributes['DW_AT_low_pc'].value
+
+                    # DWARF v4 in section 2.17 describes how to interpret the
+                    # DW_AT_high_pc attribute based on the class of its form.
+                    # For class 'address' it's taken as an absolute address
+                    # (similarly to DW_AT_low_pc); for class 'constant', it's
+                    # an offset from DW_AT_low_pc.
+                    highpc_attr = DIE.attributes['DW_AT_high_pc']
+                    highpc_attr_class = describe_form_class(highpc_attr.form)
+                    if highpc_attr_class == 'address':
+                        highpc = highpc_attr.value
+                    elif highpc_attr_class == 'constant':
+                        highpc = lowpc + highpc_attr.value
+                    else:
+                        print('Error: invalid DW_AT_high_pc class:',
+                              highpc_attr_class)
+                        continue
+
+                    if lowpc <= address < highpc:
+                        return DIE.attributes['DW_AT_name'].value.decode('latin-1')
+            except KeyError:
+                continue
+    return None
+
+def get_function_address_range(dwarfinfo, func):
+    # Go over all DIEs in the DWARF information, looking for a subprogram
+    # entry with an address range that includes the given address. Note that
+    # this simplifies things by disregarding subprograms that may have
+    # split address ranges.
+    for CU in dwarfinfo.iter_CUs():
+        for DIE in CU.iter_DIEs():
+            try:
+                if DIE.tag == 'DW_TAG_subprogram' and DIE.attributes['DW_AT_name'].value.decode('latin-1') == func:
+                    lowpc = DIE.attributes['DW_AT_low_pc'].value
+
+                    # DWARF v4 in section 2.17 describes how to interpret the
+                    # DW_AT_high_pc attribute based on the class of its form.
+                    # For class 'address' it's taken as an absolute address
+                    # (similarly to DW_AT_low_pc); for class 'constant', it's
+                    # an offset from DW_AT_low_pc.
+                    highpc_attr = DIE.attributes['DW_AT_high_pc']
+                    highpc_attr_class = describe_form_class(highpc_attr.form)
+                    if highpc_attr_class == 'address':
+                        highpc = highpc_attr.value
+                    elif highpc_attr_class == 'constant':
+                        highpc = lowpc + highpc_attr.value
+                    else:
+                        print('Error: invalid DW_AT_high_pc class:',
+                              highpc_attr_class)
+                        continue
+
+                    return (lowpc, highpc)
+            except KeyError:
+                continue
+    return None
---
+++ tools/memdump.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+# Trace a specific function, and optionally sample the value of specified registers
+#
+# Usage:
+# functrace.py --bpf-func <func name to trace> --bpf-regs <regs to use> <other arguments...>
+#
+# For example:
+# functrace.py --bpf-func __rg_alloc --bpf-regs a1 --bin binaryfile
+
+import sys
+import os
+sys.path.append(os.path.join(
+  os.path.dirname(os.path.abspath(__file__)), "..", "contrib"))
+
+from debugger import build_debugger_process, locate_bin, extract_bpf_arg
+from elf import decode_funcname
+from bcc import BPF, USDT
+import ctypes
+import re
+
+from elftools.elf.elffile import ELFFile
+
+elf = ELFFile(open(locate_bin(), "rb"))
+
+func_name = extract_bpf_arg("func")
+regs = extract_bpf_arg("regs").split(",")
+
+symtab = elf.get_section_by_name(".symtab")
+
+entries = list(filter(lambda symbol: (re.search(func_name, symbol.name)) and
+                                     (symbol.entry.st_info.type == "STT_FUNC"),
+                      symtab.iter_symbols()))
+
+if len(entries) > 1:
+  print("There is more than one entry matching %s, please include more chars to narrow down the search:" % (func_name))
+  for entry in entries:
+    print(entry.name)
+  exit(1)
+
+func_name = entries[0].name
+address = entries[0].entry.st_value
+
+print("Profiling func %s" % (func_name))
+
+bpf_text = """
+#include "riscv.h"
+
+BPF_HASH(stats, uint64_t);
+
+@@DEFS@@
+
+int do_execute(struct pt_regs *ctx) {
+    uint64_t pc;
+    bpf_usdt_readarg(1, ctx, &pc);
+
+    if (pc != @@PC@@) {
+      return 0;
+    }
+
+    uint64_t regs_addr;
+    bpf_usdt_readarg(4, ctx, &regs_addr);
+
+    uint64_t mem_addr;
+    bpf_usdt_readarg(5, ctx, &mem_addr);
+
+    stats.increment(1);
+
+    @@ACTIONS@@
+
+    return 0;
+}
+
+int do_execute_end(struct pt_regs *ctx) {
+    uint64_t pc;
+    bpf_usdt_readarg(1, ctx, &pc);
+
+    if (pc != @@PC@@) {
+      return 0;
+    }
+
+    uint64_t regs_addr;
+    bpf_usdt_readarg(4, ctx, &regs_addr);
+
+    uint64_t mem_addr;
+    bpf_usdt_readarg(5, ctx, &mem_addr);
+
+    stats.increment(1);
+
+    @@ACTIONS@@
+
+    return 0;
+}
+"""
+
+bpf_text = bpf_text.replace("@@PC@@", str(address))
+
+def_text = "\n".join(map(lambda reg: "BPF_HASH(hash_%s);\nBPF_HASH(hash_mem_%s);" % (reg, reg), regs))
+action_text = "\n".join(map(lambda reg: """
+uint64_t reg_{reg};
+bpf_probe_read_user(&reg_{reg}, sizeof(uint64_t), (void *)(regs_addr + 8 * {reg_up}));
+uint64_t mem_{reg};
+bpf_probe_read_user(&mem_{reg}, sizeof(uint64_t), (void *)(mem_addr + reg_{reg}));
+hash_{reg}.increment(reg_{reg});
+hash_mem_{reg}.update(&reg_{reg}, &mem_{reg});
+""".format(reg=reg, reg_up=reg.upper()), regs))
+
+bpf_text = bpf_text.replace("@@DEFS@@", def_text).replace("@@ACTIONS@@", action_text)
+print(bpf_text)
+
+p = build_debugger_process()
+
+u = USDT(pid=int(p.pid))
+u.enable_probe(probe="ckb_vm:execute_inst", fn_name="do_execute")
+include_path = os.path.join(
+  os.path.dirname(os.path.abspath(__file__)), "..", "bpfc")
+b = BPF(text=bpf_text, usdt_contexts=[u], cflags=["-Wno-macro-redefined", "-I", include_path])
+
+p.communicate(input="\n".encode())
+print()
+print()
+
+called = b["stats"][ctypes.c_ulong(1)].value
+print("Func %s has been called %s times!" % (func_name, called))
+
+for reg in regs:
+  table_name = "hash_%s" % (reg)
+  print("Stats for %s:" % (table_name))
+  table = b.get_table(table_name)
+  count = sum([v.value for _, v in table.items()])
+  print("count: ", count)
+  possibly_double_freed = [k for k, v in table.items() if v.value > 1 and k.value != 0]
+  for k, v in sorted(table.items(), key=lambda kv: kv[0].value):
+      if v.value > 1 and k.value != 0:
+          print(f"key: {k.value:016x}, value: {v.value:}")
+  table_name = "hash_mem_%s" % (reg)
+  print("Stats for %s:" % (table_name))
+  table = b.get_table(table_name)
+  for k in possibly_double_freed:
+      print(k, table[k])
